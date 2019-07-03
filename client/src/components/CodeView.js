c4cc65305287d84358b5e4e6380bc6d08d3e04e3
simengangstad <simen.gangstad@me.com>
2019-07-03 08:37:34 +0200

    Use custom code style

--- client/src/components/CodeView.js
+++ client/src/components/CodeView.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
+import "../styles.css";
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { prism } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { Default } from './CodeStyle';
 
 // Code map is a map of language as keys and the code as the value for the respective
 // languages
@@ -17,17 +18,14 @@ class CodeView extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { currentLanguage: "ruby" };
+    this.state = { currentLanguage: "python" };
   }
 
   render() {
-
-    console.log(this.state.currentLanguage);
-
     return (
       // Use the default Vipps card and enrich it with the syntax highlighted code for the current language
-      <div className="white-bg card padding-l">      
-        <SyntaxHighlighter language={this.state.currentLanguage} style={prism}>
+      <div className="white-bg card padding-l">
+        <SyntaxHighlighter language={this.state.currentLanguage} style={Default}>
           {this.props.codeMap[this.state.currentLanguage]}
         </SyntaxHighlighter>
       </div>
