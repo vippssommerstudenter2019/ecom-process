577368e77fb5478a34c699c4cf609a7592708396
simengangstad <simen.gangstad@me.com>
2019-07-04 10:13:31 +0200

    Add footer

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Content from "./components/Content"
 import { StickyContainer } from 'react-sticky';
 import './App.css';
 import { Sections } from "./Model";
+import Footer from "./components/Footer";
 
 class App extends React.Component {
 
@@ -16,8 +17,9 @@ class App extends React.Component {
 				<StickyContainer>
 					<Content activeLanguage="python" url="https://raw.githubusercontent.com/vippsas/vipps-ecom-api/master/docs/swagger.yaml" sections={Sections}/>
 				</StickyContainer>
+				<Footer />
 			</div>
-		);
+		)	
 	}
 }
 
---
+++ client/src/components/Footer.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import vippsSmile from '../assets/images/vipps-smile.svg';
+
+const Footer = () => (
+    <footer className="footer">
+        <div className="grid-container">
+            <div className="grid-x  grid-padding-x">
+                <div className="cell  small-11  small-offset-1  medium-1  medium-offset-0  large-1">
+                    <img className="footer__logo" src={vippsSmile} alt="" />
+                </div>
+                <div className="cell  small-11  small-offset-1  medium-3  medium-offset-0  large-3">
+                    <p className="footer__vision">Vipps AS - laget i Norge, med kjærlighet for forenkling.</p>
+                </div>
+                <div className="cell  small-11  small-offset-1  medium-3  medium-offset-1  large-3  large-offset-2">
+                    <nav>
+                        <ul className="footer-list">
+                            <li className="footer-list__item">
+                                <a className="footer-list__link" href="https://www.vipps.no/om-oss/">
+                                    Om Vipps
+                </a>
+                            </li>
+                            <li className="footer-list__item">
+                                <a className="footer-list__link" href="https://www.vipps.no/om-oss/jobb-hos-oss/">
+                                    Jobb i Vipps
+                </a>
+                            </li>
+                            <li className="footer-list__item">
+                                <a className="footer-list__link" href="https://www.vipps.no/kontakt-oss/">
+                                    Kontakt oss
+                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+                <div className="cell  small-11  small-offset-1  medium-4  medium-offset-0  large-3">
+                    <nav>
+                        <ul className="footer-list">
+                            <li className="footer-list__item">
+                                <a className="footer-list__link" href="https://www.vipps.no/developer/">
+                                    For utviklere
+                </a>
+                            </li>
+                            <li className="footer-list__item">
+                                <a className="footer-list__link" href="https://www.vipps.no/vis-at-du-tar-vipps/">
+                                    Markedsmateriell
+                </a>
+                            </li>
+                            <li className="footer-list__item">
+                                <a
+                                    className="footer-list__link"
+                                    href="https://www.vipps.no/personvern-og-vilk%C3%A5r/"
+                                >
+                                    Personvern og vilkår
+                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </footer>
+);
+
+export default Footer;
