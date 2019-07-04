7e1200df3bfb58b7991ae876c1318e5420061ef3
simengangstad <simen.gangstad@me.com>
2019-07-04 15:23:45 +0200

    Make elements align vertically

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import lottie from 'lottie-web';
-import "../App.css";
 
 const getAnimationTimeout = () => {
     return Math.floor(Math.random() * 10000);
@@ -37,27 +36,30 @@ class Header extends React.Component {
                 data-sticky
                 data-margin-top="0">
 
-                <div className="grid-container">
-                    <div className="grid-x header fixed-height align-justify">
-                        <div className="cell shrink align-self-middle">
-                            <a href="/">
-                                <div className="vipps-logo-wrapper">
-                                    <div id="vipps-logo" />
-                                </div>
-                            </a>
-                        </div>
-                        <div class="cell  small-12  medium-auto  medium-text-right">
-                            <nav id="menu" class="show-for-medium">
-                                <ul class="menu  vertical  medium-horizontal">
-
-                                    <li class="menu__item  "><a href="/produkter-og-tjenester/">Produkter og tjenester</a></li>
-                                    <li class="menu__item  "><a href="/hjelp/">Hjelp</a></li>
-                                    <li class="menu__item  menu__item--login"><a href="/logg-inn/">Logg inn</a></li>
-                                </ul>
-                            </nav>
-                        </div>
+                <div class="grid-x header fixed-height align-justify">
+                    <div class="cell shrink align-self-middle">
+                        <a href="/">
+                            <div className="vipps-logo-wrapper">
+                                <div id="vipps-logo" ß/>
+                            </div>
+                            <span class="show-for-sr">Gå til forsiden</span>
+                        </a>
                     </div>
-                </div>
+                    <div class="cell  shrink   show-for-small-only">
+                         <button data-toggle="collapse" aria-controls="menu" aria-expanded="false" aria-label="Vis meny">
+                                Meny
+                            </button>
+                    </div>
+                    <div class="cell  small-12  medium-auto  medium-text-right align-self-middle">
+                        <nav id="menu" class="show-for-medium">
+                            <ul class="menu  vertical  medium-horizontal">
+                                <li class="menu__item  "><a href="/produkter-og-tjenester/">Produkter og tjenester</a></li>
+                                <li class="menu__item  "><a href="/hjelp/">Hjelp</a></li>
+                                <li class="menu__item  menu__item--login"><a href="/logg-inn/">Logg inn</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+               </div>
             </header>
         );
     }
