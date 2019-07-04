e4e14529a20b85e19267c959f4f70e2e9e33728f
Elias Ramsgaard <elias.ramsgaard@vipps.no>
2019-07-04 17:12:34 +0200

    Grids are back!

--- client/src/components/Step.js
+++ client/src/components/Step.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Element} from 'react-scroll';
+//import {Element} from 'react-scroll';
 import CodeView from "./CodeView";
 //import Img from "./ecom-steps/Step1.svg";
 
@@ -9,7 +9,7 @@ Wrapper for future content: code + text + image
 ScrollLinking
 */
 
-const titleClass = "step-title xlarge-font-size text-color-black";
+const titleClass = "xlarge-font-size text-color-black";
 const descriptionClass = "default-font-size text-color-black";
 
 class Step extends Component {
@@ -27,28 +27,26 @@ class Step extends Component {
 	*/
 	
 	render() {
-		const imgflt = "step-float-" +(this.props.position === 'left')? "right" : "left"
-		const txtflt = "step-float-" + this.props.position;
-
-		const img = this.props.imagelink;
-		const code = this.props.code;
-		const title = this.props.title;
-		const description = this.props.description;
+		const imgflt = "step-img-" + ((this.props.position === 'left')? "right" : "left");
 		
 		return (
 			<div className="step-wrapper" id={this.props.scrollId}>
-				<div className={"step-text " + txtflt} vertical-align="center">
-					<div className={titleClass}>
-						{this.props.title}
+				<div className={"step-text"} vertical-align="center">
+					
+					<div className="step-sub-text">
+						<div className={titleClass}>{this.props.title}</div>
 					</div>
-					<div className="step-pad" />
-					<div className={descriptionClass}>
-						{this.props.description}
+					
+					<div className="step-sub-text">
+						<div className={descriptionClass}>{this.props.description}</div>
 					</div>
+					
 				</div>
-				<div className={"step-image " + imgflt} vertical-align="center">
+				
+				<div className={imgflt} vertical-align="center">
 					<img src={this.props.imagelink} />
 				</div>
+				
 			</div>
 		);
 	}
@@ -57,29 +55,27 @@ class Step extends Component {
 class CodeStep extends Component {
 	
 	render() {
-		const imgflt = "step-float-" +(this.props.position === 'left')? "right" : "left"
-		const txtflt = "step-float-" + this.props.position;
+		const imgflt = "code-img-" + ((this.props.position === 'left')? "right" : "left");
 		
 		return (
-			<div className="step-wrapper" id={this.props.scrollId}>
-				<div className="step-title">
-					<div className={"step-text " + txtflt}>
-						<div className={titleClass}>
-							{this.props.title}
-						</div>
-					</div>
+			<div className="code-wrapper" id={this.props.scrollId}>
+			
+				<div className="code-title">
+					<div className={titleClass}>{this.props.title}</div>
 				</div>
-				<div className="step-pad" />
-				<div className={"step-text " + txtflt}>
-					<div className={descriptionClass}>
-						{this.props.description}
-					</div>
-					<div className="step-pad" />
+				
+				<div className="code-description" >
+					<div className={descriptionClass}>{this.props.description}</div>
+				</div>
+				
+				<div className="code-code" >
 					<CodeView language={this.props.language} code={this.props.code} />
 				</div>
-				<div className={"step-img " + imgflt}>
+				
+				<div className={imgflt}>
 					<img src={this.props.imagelink} />
 				</div>
+				
 			</div>
 		);
 	}
