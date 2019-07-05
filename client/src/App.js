849174c3befe5ccb09dfe2c140c0ededc9b31ad6
simengangstad <simen.gangstad@me.com>
2019-07-05 10:47:16 +0200

    Wrap content-wrapper around intro and content

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Content from "./components/Content"
 import { StickyContainer } from 'react-sticky';
 import './App.css';
-import { Sections } from "./Model";
+import { Sections, Intro } from "./Model";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import IntroBox from "./components/IntroBox";
 
 class App extends React.Component {
 
-  constructor(props) {
+	constructor(props) {
 		super(props);
 	}
 
@@ -17,11 +18,16 @@ class App extends React.Component {
 			<div className="App">
 				<Header />
 				<StickyContainer>
-					<Content activeLanguage="python" url="https://raw.githubusercontent.com/vippsas/vipps-ecom-api/master/docs/swagger.yaml" sections={Sections}/>
+					<div className="content-wrapper">
+						<div className="content-area">
+							<IntroBox content={Intro} />
+							<Content activeLanguage="python" url="https://raw.githubusercontent.com/vippsas/vipps-ecom-api/master/docs/swagger.yaml" sections={Sections} />
+						</div>
+					</div>
 				</StickyContainer>
 				<Footer />
 			</div>
-		)	
+		)
 	}
 }
 
