1fbf1eb99d74422cdbeba93044fd9cf9a55091a8
simengangstad <simen.gangstad@me.com>
2019-07-09 18:43:43 +0200

    Remove see api documentaiton for keyword fields

--- client/src/Model.js
+++ client/src/Model.js
@@ -5,8 +5,8 @@ const Sections = [
         "description": "A customer wants to buy something online and chooses Vipps as their payment method. For this to be possible, the merchant needs a [valid token]. The merchant needs to renew the token every [24 hours].",
         "img" : "../assets/ecom-steps/Step1.svg",
         "keywords" : {
-            "valid token": "A token is an object which represents the right to perform some operation. An Access token, is a system object representing the subject of access control operations. All API calls are authenticated and authorized based on the application access token (JWT bearer token) and a subscription key. See API documentation for more info",
-            "24 hours": "The access token is valid for 1 hour in Merchant Test (MT) and 24 hours in Production. This is done for various security reasons. See API documentation for more info."
+            "valid token": "A token is an object which represents the right to perform some operation. An Access token, is a system object representing the subject of access control operations. All API calls are authenticated and authorized based on the application access token (JWT bearer token) and a subscription key.",
+            "24 hours": "The access token is valid for 1 hour in Merchant Test (MT) and 24 hours in Production. This is done for various security reasons."
         }
    },
    {
@@ -15,7 +15,7 @@ const Sections = [
        "description": "The merchant provides Vipps with [price and ID] for items that are being sold",
        "img": "../assets/ecom-steps/Step2.svg",
        "keywords": {
-           "price and ID": "Price and ID is included in the Initiate request body, and no information about the customer is needed. See API documentation for more info.",
+           "price and ID": "Price and ID is included in the Initiate request body, and no information about the customer is needed. ",
        }
    },
    {
@@ -24,8 +24,8 @@ const Sections = [
        "description": "For mobile and desktop browsers, integration is handled by Vipps using the Vipps [landing page].\nIf customer is in an application using Vipps as a service, the Vipps app is triggered using [app-switch].",
        "img": "../assets/ecom-steps/Step3.svg",
        "keywords": {
-           "landing page": "The Vipps landing page is mandatory, and provides a consistent and recognizable user experience, that helps guide the user through the payment flow. In this way Vipps takes responsibility for helping the user from the browser to the app, and to complete the payment in a familiar way. See more in API Documentation",
-           "app-swich": "The Vipps landing page is mandatory, and provides a consistent and recognizable user experience, that helps guide the user through the payment flow. In this way Vipps takes responsibility for helping the user from the browser to the app, and to complete the payment in a familiar way. See more in API Documentation",
+           "landing page": "The Vipps landing page is mandatory, and provides a consistent and recognizable user experience, that helps guide the user through the payment flow. In this way Vipps takes responsibility for helping the user from the browser to the app, and to complete the payment in a familiar way. ",
+           "app-swich": "The Vipps landing page is mandatory, and provides a consistent and recognizable user experience, that helps guide the user through the payment flow. In this way Vipps takes responsibility for helping the user from the browser to the app, and to complete the payment in a familiar way. ",
        }
    },
    {
@@ -34,7 +34,7 @@ const Sections = [
        "description": "A reservation is [cancelled] if the user rejects the initiated payment in the Vipps app.\nAlso, due to safety reasons, a cancellation will be triggered if the user does not respond within 5 to 10 minutes.",
        "img": "../assets/ecom-steps/Step4.svg",
        "keywords": {
-           "cancelled": "The Vipps landing page is mandatory, and provides a consistent and recognizable user experience, that helps guide the user through the payment flow. In this way Vipps takes responsibility for helping the user from the browser to the app, and to complete the payment in a familiar way. See more in API Documentation",
+           "cancelled": "The Vipps landing page is mandatory, and provides a consistent and recognizable user experience, that helps guide the user through the payment flow. In this way Vipps takes responsibility for helping the user from the browser to the app, and to complete the payment in a familiar way. ",
        }
    },
    {
@@ -43,9 +43,9 @@ const Sections = [
        "description": "If everything goes smoothly, an [amount is reserved] in the customers bank account. This means that the money is on hold, and can not be used or withdrawn by the customer. According to [Norwegian law], the merchant cannot capture the money before the item or service is shipped or delivered to the customer. This is to secure a safe payment! A [confirmation] of the reservation is sent to the merchant.",
        "img": "../assets/ecom-steps/Step5.svg",
        "keywords": {
-           "amount is reserved": "The Vipps landing page is mandatory, and provides a consistent and recognizable user experience, that helps guide the user through the payment flow. In this way Vipps takes responsibility for helping the user from the browser to the app, and to complete the payment in a familiar way. See more in API Documentation",
-           "Norwegian law": "The Vipps landing page is mandatory, and provides a consistent and recognizable user experience, that helps guide the user through the payment flow. In this way Vipps takes responsibility for helping the user from the browser to the app, and to complete the payment in a familiar way. See more in API Documentation",
-           "confirmation": "The Vipps landing page is mandatory, and provides a consistent and recognizable user experience, that helps guide the user through the payment flow. In this way Vipps takes responsibility for helping the user from the browser to the app, and to complete the payment in a familiar way. See more in API Documentation",
+           "amount is reserved": "The Vipps landing page is mandatory, and provides a consistent and recognizable user experience, that helps guide the user through the payment flow. In this way Vipps takes responsibility for helping the user from the browser to the app, and to complete the payment in a familiar way. ",
+           "Norwegian law": "The Vipps landing page is mandatory, and provides a consistent and recognizable user experience, that helps guide the user through the payment flow. In this way Vipps takes responsibility for helping the user from the browser to the app, and to complete the payment in a familiar way. ",
+           "confirmation": "The Vipps landing page is mandatory, and provides a consistent and recognizable user experience, that helps guide the user through the payment flow. In this way Vipps takes responsibility for helping the user from the browser to the app, and to complete the payment in a familiar way. ",
        }
     },
     {
@@ -54,7 +54,7 @@ const Sections = [
         "description": "As the customer receives the [order confirmation], the transaction is accepted and the order is processed in the merchant systems.",
         "img": "../assets/ecom-steps/Step6.svg",
         "keywords": {
-            "order confirmation": "The Vipps landing page is mandatory, and provides a consistent and recognizable user experience, that helps guide the user through the payment flow. In this way Vipps takes responsibility for helping the user from the browser to the app, and to complete the payment in a familiar way. See more in API Documentation",
+            "order confirmation": "The Vipps landing page is mandatory, and provides a consistent and recognizable user experience, that helps guide the user through the payment flow. In this way Vipps takes responsibility for helping the user from the browser to the app, and to complete the payment in a familiar way. ",
         }
     },
     {
@@ -63,8 +63,8 @@ const Sections = [
         "description": "When an item or service is shipped or delivered, the [merchant notifies Vipps] that the order is completed. This is when Vipps and the merchant can make a [capture] of the reserved amount.",
         "img": "../assets/ecom-steps/Step7.svg",
         "keywords": {
-            "merchant notifies Vipps": "The Vipps landing page is mandatory, and provides a consistent and recognizable user experience, that helps guide the user through the payment flow. In this way Vipps takes responsibility for helping the user from the browser to the app, and to complete the payment in a familiar way. See more in API Documentation",
-            "capture": "The Vipps landing page is mandatory, and provides a consistent and recognizable user experience, that helps guide the user through the payment flow. In this way Vipps takes responsibility for helping the user from the browser to the app, and to complete the payment in a familiar way. See more in API Documentation",
+            "merchant notifies Vipps": "The Vipps landing page is mandatory, and provides a consistent and recognizable user experience, that helps guide the user through the payment flow. In this way Vipps takes responsibility for helping the user from the browser to the app, and to complete the payment in a familiar way. ",
+            "capture": "The Vipps landing page is mandatory, and provides a consistent and recognizable user experience, that helps guide the user through the payment flow. In this way Vipps takes responsibility for helping the user from the browser to the app, and to complete the payment in a familiar way. ",
         }
     },
     {
@@ -73,7 +73,7 @@ const Sections = [
         "description": "If the item is sent in return, the merchant wants to [refund] the captured amount. To initiate a refund, the merchant request Vipps to refund and the amount will be in the customers bank account after 2-3 working days.",
         "img": "../assets/ecom-steps/Step8.svg",
         "keywords": {
-            "[refund]": "The Vipps landing page is mandatory, and provides a consistent and recognizable user experience, that helps guide the user through the payment flow. In this way Vipps takes responsibility for helping the user from the browser to the app, and to complete the payment in a familiar way. See more in API Documentation",
+            "[refund]": "The Vipps landing page is mandatory, and provides a consistent and recognizable user experience, that helps guide the user through the payment flow. In this way Vipps takes responsibility for helping the user from the browser to the app, and to complete the payment in a familiar way. ",
         }
     }
 ];
