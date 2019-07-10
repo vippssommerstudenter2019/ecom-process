b344f2477ee2e078012894ebd8ecf14646ac13c8
simengangstad <simen.gangstad@me.com>
2019-07-10 11:10:53 +0200

    Clean up code view

--- client/src/components/CodeView.js
+++ client/src/components/CodeView.js
@@ -1,6 +1,6 @@
+import "../App.css";
 import React, { Component } from 'react';
 import { PropTypes } from 'prop-types';
-import "../App.css";
 import { Controlled as CodeMirror } from 'react-codemirror2';
 import 'codemirror/mode/python/python';
 import 'codemirror/mode/javascript/javascript';
@@ -9,8 +9,6 @@ import 'codemirror/mode/go/go';
 import 'codemirror/mode/http/http';
 import 'codemirror/mode/shell/shell';
 import 'codemirror/mode/ruby/ruby';
-//import 'codemirror/mode/-lang-/-lang-'; To support other languages!!
-import DropDownPicker from './DropDownPicker.js';
 
 const propTypes = {
 	language: PropTypes.string.isRequired,
@@ -18,32 +16,38 @@ const propTypes = {
 };
 
 const langmap = {
-	"java"	: "text/x-java",
-	"go"	: "text/x-go",
+	"java": "text/x-java",
+	"go": "text/x-go",
 }
 
 class CodeView extends Component {
+
 	constructor(props) {
 		super(props);
-		this.state = {
-			value: true,
-		};
 
 		this.handleCopyClick = this.handleCopyClick.bind(this);
 	}
 
+	/**
+	 * Handles the click when the user wants to copy the code.
+	 */
 	handleCopyClick() {
+
+		// We create a fake text area and inject the code into it
 		var textArea = document.createElement("textarea");
 		textArea.value = this.props.code;
 		document.body.appendChild(textArea);
 		textArea.focus();
 		textArea.select();
 
+		// Try to copy from that text area
 		try {
+
 			document.execCommand("copy");
+
+			// TODO: Implement overlay for successful copy when design is ready
 			//var successful = document.execCommand('copy');
 			// var msg = successful ? 'Copied' : 'Couldn\'t copy';
-			// TODO: Implement overlay for successful copy when design is ready
 		} catch (err) {
 			console.error('Fallback: Oops, unable to copy', err);
 		}
@@ -51,49 +55,45 @@ class CodeView extends Component {
 		document.body.removeChild(textArea);
 	}
 
-	
-  render() {
-	const language  = this.props.language;
-	const languages = this.props.languages;
-	const langcall  = this.props.langcall;
-	//console.log("CodeView", langcall);
-	
-	let items = [];
-	Array.from(languages, (val, i) => {
-		if (val === language) items.push(<button className="selected"> {val} </button>);
-		else items.push(<button onClick={()=>langcall(val)}> {val} </button>);
-		return val;
-	});
-	
-	const code = this.props.code;
-	const options = {
-		mode: langmap[language] || language,
-		theme: 'vipps',
-		lineNumbers: true,
-		lineWrapping: true,
-	};
-	const onChange = (editor, data, value) => {console.log(editor.options.mode);};
-		//<DropDownPicker choices={languages} selected={language} callback={langcall} />
-		return (
 
-			// Use the default Vipps card and enrich it with the syntax highlighted code for the current language
+	render() {
+
+		// The languages to choose from.
+		let items = [];
+
+		// Populate the utility bar with languages to choose
+		Array.from(this.props.languages, (val, i) => {
+			if (val === this.props.language) items.push(<button className="selected"> {val} </button>);
+			else items.push(<button onClick={() => this.props.langcall(val)}> {val} </button>);
+			return val;
+		});
+
+		// Settings for code mirror
+		const options = {
+			mode: langmap[this.props.language] || this.props.language,
+			theme: 'vipps',
+			lineNumbers: true,
+			lineWrapping: true,
+		};
+
+		return (
+			// Render a code mirror with an utility bar and style it according to Vipps style.
 			<div className="small-font-size">
-				<div className="code-top">
-					<div className="code-top-left">
+				<div className="codeview-utility-bar">
+					<div className="codeview-utility-bar-left">
 						{items}
-					</div>	
-					<div className="code-top-right">
-						<button className="codeview-button" onClick={this.handleCopyClick}>Copy</button>
+					</div>
+					<div className="codeview-utility-bar-right">
+						<button onClick={this.handleCopyClick}>Copy</button>
 					</div>
 				</div>
-					<CodeMirror
-						editorDidMount={ed => ed.refresh()}
-						value={this.props.code}
-						options={options}
-						onChange={onChange}
-						scrollbarStyle="null">
-					</CodeMirror>
-		</div>
+				<CodeMirror
+					editorDidMount={ed => ed.refresh()}
+					value={this.props.code}
+					options={options}
+					scrollbarStyle="null">
+				</CodeMirror>
+			</div>
 		);
 	}
 }
