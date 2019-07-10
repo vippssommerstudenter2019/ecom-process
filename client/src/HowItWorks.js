1b9089348bf73ebe6c5a868b698f16dcc8f72bc6
simengangstad <simen.gangstad@me.com>
2019-07-10 14:20:14 +0200

    Use how it works stylesheet

--- client/src/HowItWorks.js
+++ client/src/HowItWorks.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Content from "./components/Content"
 import { StickyContainer } from 'react-sticky';
-import './styles/App.css';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import IntroBox from "./components/IntroBox";
+
+import './styles/how-it-works.css';
 
 class HowItWorks extends React.Component {
 	render() {
@@ -17,7 +18,7 @@ class HowItWorks extends React.Component {
 							<IntroBox content={this.props.intro} />
 						</div>
 						<div className="step-area">
-							<Content url="https://raw.githubusercontent.com/vippsas/vipps-ecom-api/master/docs/swagger.yaml" sections={this.props.sections} />
+							<Content url="https://raw.githubusercontent.com/vippsas/vipps-ecom-api/master/docs/swagger.yaml" sections={this.props.sections}/>
 						</div>
 					</div>
 				</StickyContainer>
