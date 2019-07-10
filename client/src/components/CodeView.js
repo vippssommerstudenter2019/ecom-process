3873ca1f998607323cf7c546b54d22f81ef2286f
simengangstad <simen.gangstad@me.com>
2019-07-10 10:00:57 +0200

    Add custom button for codeview

--- client/src/components/CodeView.js
+++ client/src/components/CodeView.js
@@ -54,7 +54,7 @@ class CodeView extends Component {
 			<div className="small-font-size">
 				<div className="code-top">
 					<div className="code-top-right">
-						<button className="button tiny hollow" onClick={this.handleCopyClick}>Copy</button>
+						<button className="codeview-button" onClick={this.handleCopyClick}>Copy</button>
 					</div>
 				</div>
 					<CodeMirror
